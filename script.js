14f2d1e9061bf839303df477de0ae5c793c00ce8
Neli <illena@centrum.cz>
2018-06-27 19:07:26 +0200

    add content

--- script.js
+++ script.js
@@ -1,30 +1,10 @@
 var map;
-
+var markers = [];
+var contentString = "";
 var prague = {lat: 50.08804, lng: 14.42076};
 var cagliari = {lat: 39.23054, lng: 9.11917};
 
 // var sourceData = [prague, cagliari];
-
-var contentString = '<div id="content">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-      '<div id="bodyContent">'+
-      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-      'sandstone rock formation in the southern part of the '+
-      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-      'south west of the nearest large town, Alice Springs; 450&#160;km '+
-      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-      'Aboriginal people of the area. It has many springs, waterholes, '+
-      'rock caves and ancient paintings. Uluru is listed as a World '+
-      'Heritage Site.</p>'+
-      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-      'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-      '(last visited June 22, 2009).</p>'+
-      '</div>'+
-      '</div>';
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
@@ -39,6 +19,7 @@ function initMap() {
 
 function addMapContent() {
   var infowindow = new google.maps.InfoWindow({
+    // content: getContent(sourceData[i])
     content: contentString
   });
 
@@ -48,7 +29,7 @@ function addMapContent() {
     console.log(sourceData[i].longitude);
     var marker = new google.maps.Marker({
       position: {
-        lat: sourceData[i].lattitude,
+        lat: sourceData[i].latitude,
         lng: sourceData[i].longitude
       },
       map: map,
@@ -61,9 +42,16 @@ function addMapContent() {
     //   console.log(marker);
     // });
 
-    marker.addListener('click', function() {
-      infowindow.open(map, marker);
+    console.log(sourceData[i]);
+    infowindow.setContent(getContent(sourceData[i]));
+
+     google.maps.event.addListener(marker, 'click', function() {
+
+      infowindow.open(map, this);
+      console.log('test');
     });
+
+    markers.push(marker);
   }
 
 
@@ -78,4 +66,10 @@ function addMapContent() {
   //   map: map
   // });
 
+}
+
+function getContent(sourceData) {
+  console.log(sourceData);
+  var contentString = `<h2>${sourceData.Name}</h2><p>${sourceData.info}</p>`;
+  return contentString;
 }
--- sourceData.js
+++ sourceData.js
@@ -2,11 +2,14 @@ var sourceData = [
  {
    Name: "Prague",
    latitude: 50.08804,
-   longitude: 14.42076
+   longitude: 14.42076,
+   info: "Capital of the Czech Republic"
  },
  {
    Name: "Cagliari",
    latitude: 39.23054,
-   longitude: 9.11917
+   longitude: 9.11917,
+   info: "Capital of Sardinia"
+
  }
 ]
